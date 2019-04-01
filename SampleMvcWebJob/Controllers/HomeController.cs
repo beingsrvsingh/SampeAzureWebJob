65f8cb26842d7728d1dc8953d6b020f3be4358cb
samsaurav <sam.saurav@ziorinfo.com>
2019-04-01 22:53:21 +0530

    Existing project commit

---
+++ SampleMvcWebJob/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMvcWebJob.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://samplewebjobservice.scm.azurewebsites.net/api/");
+            // the creds from my .publishsettings file
+            //var byteArray = Encoding.ASCII.GetBytes("username:password");
+
+            string userName = "$SampleWebJobService";
+            string userPassword = "mQ4cRQeuCshZhltdsB5ZD8jhgwriz9uxv1S4fr6G3929pvYDcGuocCS0cyfh";
+            var base64Auth = Convert.ToBase64String(Encoding.Default.GetBytes($"{userName}:{userPassword}"));
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Auth);
+            // POST to the run action for my job
+            var response = await client.PostAsync("triggeredwebjobs/WebJobAwesome/run", null);
+
+            return View();
+            //return Content("Welcome Web Jobs");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(int?id)
+        {
+            string directory = System.IO.Directory.GetParent(Environment.CurrentDirectory).ToString();
+            //Process.Start("C://Users//Saurav//source//repos//SampleWebJob//SampleWebJob//bin//Debug//SampleWebJob.exe");
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://samplewebjobservice.scm.azurewebsites.net/api/");
+            // the creds from my .publishsettings file
+            //var byteArray = Encoding.ASCII.GetBytes("username:password");
+
+            string userName = "$SampleWebJobService";
+            string userPassword = "mQ4cRQeuCshZhltdsB5ZD8jhgwriz9uxv1S4fr6G3929pvYDcGuocCS0cyfh";
+            var base64Auth = Convert.ToBase64String(Encoding.Default.GetBytes($"{userName}:{userPassword}"));
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Auth);
+            // POST to the run action for my job
+            var response = await client.PostAsync("triggeredwebjobs/WebJobAwesome/run", null);
+
+            return Content("Welcome to Post Web Jobs");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SampleWebJob/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWebJob
+{
+    class Program
+    {
+
+       public class Mathmatics
+        {
+            public int add(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            //using (var client = new WebClient())
+            //{
+            //    var values = new NameValueCollection();
+            //    values["id"] = "1";
+
+            //    var response = client.UploadValues("http://localhost:61423/", values);
+
+            //    var responseString = Encoding.Default.GetString(response);
+            //}
+
+            //Mathmatics c = new Mathmatics();            
+            //Console.WriteLine(c.add(1, 2));
+            //Console.ReadLine();
+
+            #region Console to Mvc
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:61423/");
+            //request.Method = "GET";
+            ////specify other request properties
+
+            //try
+            //{
+            //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw ex;
+            //}
+
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:61423/");
+            //request.Method = "POST";
+            //var data = string.Format("id={0}", Uri.EscapeDataString("1"));
+            //StreamWriter requestWriter = new StreamWriter(request.GetRequestStream());
+            //requestWriter.Write(requestWriter);
+            //requestWriter.Close();
+
+            #endregion
+        }
+    }
+}
